d5d793a1aef2ce34793da8eeac32d7e626a6a6b9
darshanboyat <106928085+darshanboyat@users.noreply.github.com>
2023-09-13 19:37:02 +0530

    Fixed hire pages issue

--- components/common/HireBanner.jsx
+++ components/common/HireBanner.jsx
@@ -25,7 +25,7 @@ export default function HireBanner({
     });
     return (
         <main className="container padding-left-all-section-1 pt-28 lg:pt-32 lg:pb-24">
-            <section className={`grid lg:grid-cols-2 grid-cols-1 ${type==="hire" ? 'gap-x-24' : 'gap-x-52'}`}>
+            <section className={`grid lg:grid-cols-2 grid-cols-1 gap-x-52`}>
                 <section>
                     <section
                         className="ml-4 lg:ml-0"
@@ -112,7 +112,7 @@ export default function HireBanner({
                             </section>
                         </section>
                     ) : (
-                        <section className={` mx-14 lg:mb-0 mb-6 bg-none ${type==='hire' ? 'max-w-[900px]' : 'max-w-[500px]'} ${type.match("php") && "drop-shadow-image"}`} data-aos="zoom-in"
+                        <section className={` mx-14 lg:mb-0 mb-6 bg-none max-w-[500px] ${type.match("php") && "drop-shadow-image"}`} data-aos="zoom-in"
                             data-aos-delay="1500">
                             <Image
                                 src={image} alt="Custom Web Development"

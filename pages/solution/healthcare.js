a9f7e5824e97ef6bf9c8dd89077fb608a274e5dc
Darshan Boyat <darshboyat@gmail.com>
2023-09-28 17:56:54 +0530

    reduce load time

--- pages/solution/healthcare.js
+++ pages/solution/healthcare.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Head from "next/head";
 import dynamic from "next/dynamic";
 const Header = dynamic(() => import("../../components/header/Header"));
@@ -8,7 +8,9 @@ const ContactForm = dynamic(() =>
   import("../../components/common/ContactForm.jsx")
 );
 const FaqHire = dynamic(() => import("../../components/common/Faqhire"));
-const Approach = dynamic(() => import("../../components/SolutionFitness/Approach.jsx"));
+const Approach = dynamic(() =>
+  import("../../components/SolutionFitness/Approach.jsx")
+);
 const SectionOne = dynamic(() =>
   import("../../components/SolutionHealthcare/SectionOne.jsx")
 );
@@ -39,9 +41,7 @@ const ProjectInquiry = dynamic(() =>
 const PlatformDevelopment = dynamic(() =>
   import("../../components/SolutionFitness/PlatformDevelopment.jsx")
 );
-const LetsKick = dynamic(() =>
-  import("../../components/common/LetsKick.js")
-);
+const LetsKick = dynamic(() => import("../../components/common/LetsKick.js"));
 const BlogArticle = dynamic(() =>
   import("../../components/common/BlogArticle.jsx")
 );
@@ -62,7 +62,8 @@ function ReactNativeDeveloper(props) {
     },
     {
       id: "3",
-      title: "Can you customize the healthcare technology to our specific needs?",
+      title:
+        "Can you customize the healthcare technology to our specific needs?",
       description:
         "Yes, we understand that every organization is unique. Our solutions are flexible and customizable to ensure they meet your specific needs.",
     },
@@ -123,7 +124,12 @@ function ReactNativeDeveloper(props) {
       "Tracks daily activity, workouts, and sleep patterns for fitness enthusiasts.",
       "Complete database of nutrition and calorie information for diet tracking.",
     ],
-    ["Over 5 million", "More than 2 million", "Over 28 million active users", "More than 200 million"],
+    [
+      "Over 5 million",
+      "More than 2 million",
+      "Over 28 million active users",
+      "More than 200 million",
+    ],
   ];
   const sectionFiveTableHead = [
     "Healthcare Application Platform",
@@ -131,87 +137,123 @@ function ReactNativeDeveloper(props) {
     "User Base",
   ];
 
-  const platformDesc = "When it comes to healthcare solution development, Brain Inventory emerges as a transformative force. With our pioneering strategies, extensive experience, and unwavering commitment to excellence, we deliver robust solutions that resonate with your users, facilitating enhanced healthcare experiences."
+  const platformDesc =
+    "When it comes to healthcare solution development, Brain Inventory emerges as a transformative force. With our pioneering strategies, extensive experience, and unwavering commitment to excellence, we deliver robust solutions that resonate with your users, facilitating enhanced healthcare experiences.";
   const platformDevelopment = [
     {
       head: "Customer Satisfaction",
-      content: "Central to our healthcare technology solutions is User Satisfaction. We believe in crafting products that not only fulfill but surpass user expectations, fostering trust and dependability. In an ever-evolving healthcare ecosystem, we stand by your side as a steadfast partner, dedicated to delivering exceptional service and innovative solutions that make a difference."
+      content:
+        "Central to our healthcare technology solutions is User Satisfaction. We believe in crafting products that not only fulfill but surpass user expectations, fostering trust and dependability. In an ever-evolving healthcare ecosystem, we stand by your side as a steadfast partner, dedicated to delivering exceptional service and innovative solutions that make a difference.",
     },
     {
       head: "Agile Procedure",
-      content: "In the dynamic landscape of the healthcare industry, agility is paramount. We recognize this imperative and tailor our solutions using agile development principles. Our approach is adaptable, efficient, and customized to meet your distinct requirements, staying attuned to shifting trends and emerging technologies. Through our agile methodology, you receive solutions that are not only relevant today but also..."
+      content:
+        "In the dynamic landscape of the healthcare industry, agility is paramount. We recognize this imperative and tailor our solutions using agile development principles. Our approach is adaptable, efficient, and customized to meet your distinct requirements, staying attuned to shifting trends and emerging technologies. Through our agile methodology, you receive solutions that are not only relevant today but also...",
     },
     {
       head: "Competitive Pricing",
-      content: "At the core of our strategy lies Competitive Costing. We understand the significance of delivering value while maintaining quality. By refining our processes, eliminating unnecessary expenses, and leveraging our expertise, we offer solutions that are both high-caliber and cost-effective. This approach empowers you to amplify your healthcare initiatives without straining..."
-    }
-  ]
-  const keyDesc = "Step into the future of healthcare solutions, where technology meets compassion to redefine patient experiences. Our mission is to reshape the healthcare landscape by integrating cutting-edge technology. With Healthcare technology, your institution can thrive in this digital era, enhancing medical services and ultimately fostering better health outcomes for all."
+      content:
+        "At the core of our strategy lies Competitive Costing. We understand the significance of delivering value while maintaining quality. By refining our processes, eliminating unnecessary expenses, and leveraging our expertise, we offer solutions that are both high-caliber and cost-effective. This approach empowers you to amplify your healthcare initiatives without straining...",
+    },
+  ];
+  const keyDesc =
+    "Step into the future of healthcare solutions, where technology meets compassion to redefine patient experiences. Our mission is to reshape the healthcare landscape by integrating cutting-edge technology. With Healthcare technology, your institution can thrive in this digital era, enhancing medical services and ultimately fostering better health outcomes for all.";
   const keyFeatures = [
     {
       head: "Effortless Workout Planning",
-      content: "The Fitness Solution streamlines workout planning. Users can easily explore customized workout routines, select preferred exercises, and schedule sessions with just a few taps. This feature enhances user convenience and optimizes their fitness journey, ensuring efficient time utilization."
+      content:
+        "The Fitness Solution streamlines workout planning. Users can easily explore customized workout routines, select preferred exercises, and schedule sessions with just a few taps. This feature enhances user convenience and optimizes their fitness journey, ensuring efficient time utilization.",
     },
     {
       head: "Real-time Virtual Coaching",
-      content: "Embracing virtual coaching, the platform offers real-time virtual training sessions between users and fitness experts. Whether for guidance, motivation, or personalized coaching, our secure and user-friendly video platform ensures that users can access expert fitness advice from the comfort of their spaces."
+      content:
+        "Embracing virtual coaching, the platform offers real-time virtual training sessions between users and fitness experts. Whether for guidance, motivation, or personalized coaching, our secure and user-friendly video platform ensures that users can access expert fitness advice from the comfort of their spaces.",
     },
     {
       head: "Effective Progress Tracking",
-      content: "Monitoring progress is essential for achieving fitness goals. Our technology simplifies progress tracking by providing visual representations of user achievements, milestones, and health improvements. This feature encourages adherence and boosts motivation to continue their fitness journey."
-    }
-  ]
-
+      content:
+        "Monitoring progress is essential for achieving fitness goals. Our technology simplifies progress tracking by providing visual representations of user achievements, milestones, and health improvements. This feature encourages adherence and boosts motivation to continue their fitness journey.",
+    },
+  ];
 
   const setionThreeCards = [
     {
       head: "The Current Landscape of Healthcare",
-      content: "Healthcare has always been a pivotal sector, but recent years have seen an unprecedented surge in its digitization. With the advent of web and mobile applications, the healthcare sector is witnessing a transformative phase. Here are some stats to back up this trend:",
+      content:
+        "Healthcare has always been a pivotal sector, but recent years have seen an unprecedented surge in its digitization. With the advent of web and mobile applications, the healthcare sector is witnessing a transformative phase. Here are some stats to back up this trend:",
       points: [
         "According to Statista, the global health market is projected to reach nearly USD 100 billion by the year 2021",
-        "The use of health and fitness apps has grown by 330% in the last three years"
-      ]
+        "The use of health and fitness apps has grown by 330% in the last three years",
+      ],
     },
     {
       head: "Impact of Online Platforms in Healthcare",
-      content: "The digital age has given rise to a new era in healthcare—an era where patients have access to health information at their fingertips, and healthcare providers have the technology to provide faster and more efficient care. The role of online platforms in healthcare is becoming increasingly crucial, as these statistics demonstrate:",
+      content:
+        "The digital age has given rise to a new era in healthcare—an era where patients have access to health information at their fingertips, and healthcare providers have the technology to provide faster and more efficient care. The role of online platforms in healthcare is becoming increasingly crucial, as these statistics demonstrate:",
       tableHead: ["Static", "Source"],
-      tableContent: [["77% of patients use search engines before booking appointments", "Think With Google"], ["Almost 60% of all healthcare visits were done virtually in 2020", "Forrester"], ["80% of internet users have searched for a health-related topic online", "Pew Internet & American Life Project"]]
-    }
-  ]
+      tableContent: [
+        [
+          "77% of patients use search engines before booking appointments",
+          "Think With Google",
+        ],
+        [
+          "Almost 60% of all healthcare visits were done virtually in 2020",
+          "Forrester",
+        ],
+        [
+          "80% of internet users have searched for a health-related topic online",
+          "Pew Internet & American Life Project",
+        ],
+      ],
+    },
+  ];
 
-  const approachDesc = "At Brain Inventory, we take a distinctive approach to crafting healthcare solutions that redefine the industry norms. Through a harmonious blend of innovation, precision, and technological prowess, we aim to create digital solutions that revolutionize healthcare interactions. Our strategy centers around translating your healthcare vision into tangible digital experiences that elevate patient care and streamline operations."
+  const approachDesc =
+    "At Brain Inventory, we take a distinctive approach to crafting healthcare solutions that redefine the industry norms. Through a harmonious blend of innovation, precision, and technological prowess, we aim to create digital solutions that revolutionize healthcare interactions. Our strategy centers around translating your healthcare vision into tangible digital experiences that elevate patient care and streamline operations.";
   const approachPoints = [
     {
       head: "In-depth Discovery and Understanding",
-      content: "Our first step in fitness solution development is gaining a deep understanding of our client’s business objectives and vision for their solutions. We believe every solution should be a direct reflection of the client’s brand and goals."
+      content:
+        "Our first step in fitness solution development is gaining a deep understanding of our client’s business objectives and vision for their solutions. We believe every solution should be a direct reflection of the client’s brand and goals.",
     },
     {
       head: "Tailored Design for Healthcare Realities",
-      content: "We conduct comprehensive market research and competitor analysis. This enables us to create a website or an app that is not just innovative, but also competitive and designed to succeed."
+      content:
+        "We conduct comprehensive market research and competitor analysis. This enables us to create a website or an app that is not just innovative, but also competitive and designed to succeed.",
     },
     {
       head: "Agile Iteration for Precise Development",
-      content: "One of the most critical aspects of our approach is designing an intuitive and engaging user experience. We ensure the developed product is user-friendly, aesthetically pleasing, and easy-to-navigate."
+      content:
+        "One of the most critical aspects of our approach is designing an intuitive and engaging user experience. We ensure the developed product is user-friendly, aesthetically pleasing, and easy-to-navigate.",
     },
     {
       head: "Aftercare",
-      content: "Our development team meticulously codes the solution, ensuring it’s robust, functional, and free from glitches. We use the latest technologies and follow best practices for website or app development."
+      content:
+        "Our development team meticulously codes the solution, ensuring it’s robust, functional, and free from glitches. We use the latest technologies and follow best practices for website or app development.",
     },
     {
       head: "Seamless Integration and Deployment",
-      content: "We conduct rigorous testing to ensure the website or application performs optimally under all conditions. Bugs are identified and fixed to ensure a smooth user experience."
+      content:
+        "We conduct rigorous testing to ensure the website or application performs optimally under all conditions. Bugs are identified and fixed to ensure a smooth user experience.",
     },
     {
       head: "Continuous Evolution and Enhancement",
-      content: "After the successful testing phase, we launch the solution on the required platforms. Post-launch, we provide continuous support and updates to keep the technology relevant and functioning flawlessly."
+      content:
+        "After the successful testing phase, we launch the solution on the required platforms. Post-launch, we provide continuous support and updates to keep the technology relevant and functioning flawlessly.",
     },
-  ]
+  ];
+  const [position, setPosition] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setPosition(true);
+    });
+  });
   return (
     <>
       <Head>
         <title>
-          Healthcare App Development Solution | Healthcare App Development Services
+          Healthcare App Development Solution | Healthcare App Development
+          Services
         </title>
         <link
           rel="canonical"
@@ -229,36 +271,63 @@ function ReactNativeDeveloper(props) {
             head1="Empowering Healthcare Digitally."
             description={sectionTwoDesc}
           />
-          <SectionThree title="Healthcare Industry Insights" sectionDesc="Healthcare is a rapidly evolving industry that is increasingly becoming technologically driven. Here are some captivating insights about the healthcare industry:" setionThreeCards={setionThreeCards}/>
-          <TiltMarquee title="HEALTHCARE THROUGH INNOVATION ✦ HEALTHCARE THROUGH INNOVATION" />
-          <SectionFour
-            title="Solutions for Healthcare Solution Development"
-            description={sectionFourDesc}
-            solutionList={solutionList}
-          />
-          <SectionFive
-            head="HEALTHCARE APPLICATIONS"
-            subhead="COMPARED TO WEBMD"
-            description={sectionFiveDesc}
-            sectionFiveTableHead={sectionFiveTableHead}
-            sectionFiveTable={sectionFiveTable}
-          />
-          <ProjectInquiry />
+          {position && (
+            <>
+              <SectionThree
+                title="Healthcare Industry Insights"
+                sectionDesc="Healthcare is a rapidly evolving industry that is increasingly becoming technologically driven. Here are some captivating insights about the healthcare industry:"
+                setionThreeCards={setionThreeCards}
+              />
+              <TiltMarquee title="HEALTHCARE THROUGH INNOVATION ✦ HEALTHCARE THROUGH INNOVATION" />
+              <SectionFour
+                title="Solutions for Healthcare Solution Development"
+                description={sectionFourDesc}
+                solutionList={solutionList}
+              />
+              <SectionFive
+                head="HEALTHCARE APPLICATIONS"
+                subhead="COMPARED TO WEBMD"
+                description={sectionFiveDesc}
+                sectionFiveTableHead={sectionFiveTableHead}
+                sectionFiveTable={sectionFiveTable}
+              />
+              <ProjectInquiry />
 
-          <PlatformDevelopment cardPoints={platformDevelopment} subhead="WHY CHOOSE BRAIN INVENTORY FOR" head="HEALTHCARE SOLUTION DEVELOPMENT" description={platformDesc}/>
-          <SectionNine brand="HEALTHCARE SOLUTION DEVELOPMENT       " pageCategory="HEALTHCARE SOLUTION DEVELOPMENT    " pageCategoryAbbr="HEALTHCARE SOLUTION DEVELOPMENT      " slow={70000} fast={55000} src="https://d1u4arv5y5eda6.cloudfront.net/images/solution/healthcare/jared-rice-NTyBbu66_SI-unsplash.png"/>
-          <PlatformDevelopment cardPoints={keyFeatures} subhead="KEY FEATURES OF" head="HEALTHCARE SOLUTION DEVELOPMENT" description={keyDesc}/>
-          <Approach title="How do we Approach Healthcare Solution Development?" desc={approachDesc} points={approachPoints}/>
-          <SectionTen/>
-          <FaqHire faq={faqdetails} />
+              <PlatformDevelopment
+                cardPoints={platformDevelopment}
+                subhead="WHY CHOOSE BRAIN INVENTORY FOR"
+                head="HEALTHCARE SOLUTION DEVELOPMENT"
+                description={platformDesc}
+              />
+              <SectionNine
+                brand="HEALTHCARE SOLUTION DEVELOPMENT       "
+                pageCategory="HEALTHCARE SOLUTION DEVELOPMENT    "
+                pageCategoryAbbr="HEALTHCARE SOLUTION DEVELOPMENT      "
+                slow={70000}
+                fast={55000}
+                src="https://d1u4arv5y5eda6.cloudfront.net/images/solution/healthcare/jared-rice-NTyBbu66_SI-unsplash.png"
+              />
+              <PlatformDevelopment
+                cardPoints={keyFeatures}
+                subhead="KEY FEATURES OF"
+                head="HEALTHCARE SOLUTION DEVELOPMENT"
+                description={keyDesc}
+              />
+              <Approach
+                title="How do we Approach Healthcare Solution Development?"
+                desc={approachDesc}
+                points={approachPoints}
+              />
+              <SectionTen />
+              <FaqHire faq={faqdetails} />
               <hr />
               <BlogArticle />
               <ContactForm />
-              {/* <LocateUs /> */}
               <LetsKick />
+              <hr />
               <Footer />
-          <hr />
-          <Footer />
+            </>
+          )}
         </section>
       </main>
     </>

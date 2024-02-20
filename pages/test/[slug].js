6026f38456765def84c32bab0a300e9c2ee45045
Darshan Boyat <darshboyat@gmail.com>
2024-02-20 11:01:23 +0530

    Test Slug Component - with all data fetching

--- pages/test/[slug].js
+++ pages/test/[slug].js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React from "react";
+import { rediss } from "../../utils/redis";
 
 const Test = ({data, blogs, page}) => {
   return <div>Test</div>;
@@ -8,6 +9,23 @@ const Test = ({data, blogs, page}) => {
 export default Test;
 
 export async function getServerSideProps(context) {
+  const postsRes = await fetch(
+    "https://braininventoryblogs.com/wordpress/index.php/wp-json/wp/v2/posts?_embed&per_page=1"
+  );
+  const totalPages = await postsRes.headers.get("X-WP-Total");
+
+  const cachedBlog = JSON.parse(await rediss.get(`blog-${context.query.slug}`));
+  if (cachedBlog) {
+    return {
+      props: {
+        data: cachedBlog.slice(0, 3),
+        blogs: cachedBlog,
+        totalPages: totalPages,
+        page: context.query.slug,
+      },
+    };
+  }
+
   const response = await axios.get(
     `https://braininventoryblogs.com/wordpress/index.php/wp-json/wp/v2/posts?_fields=id,_embedded,slug,date,title,excerpt,_links&_embed&per_page=10&page=${context.query.slug}`,
     { next: { revalidate: 600 } },
@@ -18,11 +36,19 @@ export async function getServerSideProps(context) {
       },
     }
   );
-  console.log(response.data);
+  await rediss.set(
+    `blog-${context.query.slug}`,
+    JSON.stringify(response.data),
+    "EX",
+    300
+  );
+
+  console.log(response.data)
   return {
     props: {
       data: response?.data?.slice(0, 3) ?? [],
       blogs: response?.data ?? [],
+      totalPages: totalPages,
       page: context.query.slug,
     },
   };

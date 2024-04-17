d11568122bf224799bee15d275ca95451df231cc
Binary Protagonist <63355451+binaryprotagonist@users.noreply.github.com>
2024-04-17 12:34:07 +0530

    Update mailer.js - google app 2fa update

--- utils/mailer.js
+++ utils/mailer.js
@@ -3,7 +3,7 @@ let transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: "noreply@braininventory.com",
-    pass: "Kb<mUby8TXGA3aW&",
+    pass: "yvzsguylwgjdyspo",
   },
 });
 

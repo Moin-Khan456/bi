8cac04573e32dbabc74550f9c44d5ccca3edcc71
KashifPathan <kashifpathan.kp.kp@gmail.com>
2024-05-15 11:12:45 +0530

    added portfolio schema

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Head from "next/head";
 import dynamic from "next/dynamic";
 const Header = dynamic(() => import("../components/header/Header"));
@@ -20,7 +20,9 @@ function HireAangularJsDeveloper(props) {
   return (
     <>
       <Head>
-        <title>Portfolio - Our Software Development Projects | Brain Inventory</title>
+        <title>
+          Portfolio - Our Software Development Projects | Brain Inventory
+        </title>
         <meta
           name="description"
           content="Explore our diverse portfolio of cutting-edge software solutions. Discover how our software development company has delivered innovation and excellence to clients worldwide."
@@ -40,18 +42,39 @@ function HireAangularJsDeveloper(props) {
           content="https://braininventory.s3.us-east-2.amazonaws.com/images/preview/Braininventory_custom+web+development+-+preview+image.jpg"
         />
 
-        
         <link rel="canonical" href="https://braininventory.in/portfolio" />
+
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "CollectionPage",
+              name: "Brain Inventory Portfolio",
+              url: "https://braininventory.in/portfolio",
+              description:
+                "Explore our portfolio showcasing our diverse range of projects.",
+              publisher: {
+                "@type": "Organization",
+                name: "Brain Inventory",
+                logo: {
+                  "@type": "ImageObject",
+                  url: "https://braininventory.in/_next/image?url=https%3A%2F%2Fbraininventory.s3.us-east-2.amazonaws.com%2Fimages%2Fmain%2FLogobg.png&w=256&q=75",
+                },
+              },
+            }),
+          }}
+        />
       </Head>
       <Header />
       <div className="container  pt-12">
         <ProjectSectionOne />
         <ProjectCards />
-            <ProjectSectionTwo />
-            <BlogArticle />
-            <ContactForm className="pl-24" />
-            <LocateUs />
-            <Footer />
+        <ProjectSectionTwo />
+        <BlogArticle />
+        <ContactForm className="pl-24" />
+        <LocateUs />
+        <Footer />
       </div>
     </>
   );

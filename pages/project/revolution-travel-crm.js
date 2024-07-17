9012265e13092e00f6b70deac03fb5c73ff1fa5c
dev-kashifpathan <k.pathan@braininventory.com>
2024-07-17 18:35:24 +0530

    fix: fixed typo in revolution-travel-crm page

--- pages/project/revolution-travel-crm.js
+++ pages/project/revolution-travel-crm.js
@@ -76,7 +76,7 @@ const Fatoura = () => {
     "Social media post scheduling on the IM platform from within the application",
   ];
   const challenge =
-    "uilding a complete travel planner with a CRM in place, we knew that we have to deal with ample Data types and manage them on DB in such a way that we can retrieve them for various reports generation with ease and fast. Integration of inbuild email management (Gmail) and Instant Messaging platforms (Insta, FB, Twitter) were some serious tasks on this project. Lastly, is to develop Drag & Drop campaign builder (No Code website builder)";
+    "Building a complete travel planner with a CRM in place, we knew that we have to deal with ample Data types and manage them on DB in such a way that we can retrieve them for various reports generation with ease and fast. Integration of inbuild email management (Gmail) and Instant Messaging platforms (Insta, FB, Twitter) were some serious tasks on this project. Lastly, is to develop Drag & Drop campaign builder (No Code website builder)";
   const solution =
     "Solution done for the challenges- Advanced-level report generation was planned in a way that we mapped the key parameters before starting the development, which helped us to efficiently use the DB. Regarding real-time IM and email management, we execute a job every 30 seconds to check new emails and messages. It was designed and planned in a way to not put any load on the server as it was running every 30 seconds. Alongside keeping the code structure optimized, used SKDs and ran the cron every for 1 minute. We made different stages and connected the node of every stage like the start and the endpoints. Added a real-time auto-save to avoid any rework for the travel agents.";
   const tech = [
